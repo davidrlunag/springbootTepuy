d0137360638d6618653dadc2b33b4824836a9090
David Luna <davidrlunag@gmail.com>
2024-02-23 11:48:58 -0600

    Proyecto Inicial

---
+++ src/test/java/com/example/tepuy/TepuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tepuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TepuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
